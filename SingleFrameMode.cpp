693ac34c595b9f1eadec3be50163b2d9b0d52240
Emaad Paracha <emaadparacha@hotmail.com>
2021-12-08 23:18:27 -0500

    fix comment

--- SingleFrameMode.cpp
+++ SingleFrameMode.cpp
@@ -4,7 +4,6 @@
  * @brief C++ Program to take a single frame from a QHYCCD camera. 
  * The program loops over different temperature, offset, gain, and exposure settings to take multiple pictures in one go.
  *
- *
  * @author Emaad Paracha
  *
  */
